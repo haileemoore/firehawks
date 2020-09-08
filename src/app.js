5092aab44ca2a3bc60b685d627c60bf06d1a955f
Hailee Doyle <haileejoanndoyle@gmail.com>
2020-09-08 14:49:09 -0500

    building react component

---
+++ src/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const e = React.createElement;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
+
+    render() {
+
+
+        return e(
+
+        );
+    }
+}
+
+ReactDOM.render( < App > < /App> , document.querySelector('main'))
